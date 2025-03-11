68b6f4ac7b650ed9e2d7af20306a3cd0ef0a103b
svetlinpopov <popov.svetlin@gmail.com>
2025-03-11 11:11:15 +0200

    new file:   public/apple-touch-icon.png 	new file:   public/browserconfig.xml 	modified:   public/favicon.ico 	new file:   public/favicon.svg 	new file:   public/favicon/android-chrome-192x192.png 	new file:   public/favicon/android-chrome-512x512.png 	new file:   public/favicon/favicon-16x16.png 	new file:   public/favicon/favicon-32x32.png 	new file:   public/favicon/mstile-144x144.png 	new file:   public/favicon/mstile-150x150.png 	new file:   public/favicon/mstile-310x310.png 	new file:   public/favicon/mstile-70x70.png 	new file:   public/favicon/safari-pinned-tab.svg 	new file:   public/site.webmanifest 	modified:   resources/css/base/typography.css 	modified:   resources/css/components/dropdowns.css 	new file:   resources/fonts/inter-tight/InterTight-Italic-VariableFont_wght.ttf 	new file:   resources/fonts/inter-tight/InterTight-VariableFont_wght.ttf 	deleted:    resources/fonts/inter-tight/static/InterTight-ExtraLightItalic.ttf 	deleted:    resources/fonts/inter-tight/static/InterTight-Regular.ttf 	deleted:    resources/fonts/inter-tight/static/InterTight-SemiBold.ttf 	modified:   resources/views/partials/head.blade.php

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -3,6 +3,17 @@
 <meta name="csrf-token" content="{{ csrf_token() }}">
 <meta name="turbo-cache-control" content="no-preview">
 
+<!-- Favicons -->
+<link rel="apple-touch-icon" sizes="180x180" href="{{ asset('apple-touch-icon.png') }}">
+<link rel="icon" type="image/png" sizes="32x32" href="{{ asset('favicon/favicon-32x32.png') }}">
+<link rel="icon" type="image/png" sizes="16x16" href="{{ asset('favicon/favicon-16x16.png') }}">
+<link rel="icon" type="image/svg+xml" href="{{ asset('favicon.svg') }}">
+<link rel="manifest" href="{{ asset('site.webmanifest') }}">
+<link rel="mask-icon" href="{{ asset('favicon/safari-pinned-tab.svg') }}" color="#b3998a">
+<meta name="msapplication-config" content="{{ asset('browserconfig.xml') }}">
+<meta name="msapplication-TileColor" content="#b3998a">
+<meta name="theme-color" content="#ffffff">
+
 <title>{{ isset($title) ? "Therapetico - $title" : 'Therapetico' }}</title>
 
 <style>
